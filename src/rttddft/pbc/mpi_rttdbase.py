0dfb946ee7b9de368a6df0d264fe44293bf83c8b
Christopher Hillenbrand <chillenbrand15@gmail.com>
2026-03-17 00:50:28 -0400

    assertions

--- src/rttddft/pbc/mpi_rttdbase.py
+++ src/rttddft/pbc/mpi_rttdbase.py
@@ -406,6 +406,9 @@ class MPIKRTTDSCF(rttdbase.RTTDSCF):
 
 
         fock_init = bc.rotate_focklike(h1e + my_get_veff(dm_kpts=dm) + pp_nl_nofield)
+        for k in range(nkpts):
+            assert np.allclose(np.diag(fock_init[k]), self._scf.mo_energy[k])
+            assert np.linalg.norm(fock_init[k] - np.diag(self._scf.mo_energy[k])) < 1e-6
 
         dm = np.asarray(
             [np.diag(self._scf.mo_occ[k]) for k in range(nkpts)],
