f632a9b3904df1248d15bb208fa51644f21752df
Christopher Hillenbrand <chillenbrand15@gmail.com>
2026-03-17 00:42:08 -0400

    no pseudo

--- src/rttddft/pbc/mpi_rttdbase.py
+++ src/rttddft/pbc/mpi_rttdbase.py
@@ -393,14 +393,21 @@ class MPIKRTTDSCF(rttdbase.RTTDSCF):
             def my_get_veff(dm_kpts):
                 return self._scf.get_veff(dm_kpts=dm_kpts)
 
-        pp_nl_nofield = pseudo.pp_int.get_pp_nl(cell, kpts)
-        pp_nl_nofield3 = get_gth_pp_nl_velgauge(cell, q=np.zeros(3), kpts=kpts, vgppnl_helper=self.vgppnl_helper)
-        ppnl_err = np.linalg.norm(pp_nl_nofield-pp_nl_nofield3)
-        if ppnl_err > 1e-5:
-            raise ValueError(f"ppnl_err1={ppnl_err}")
+        if cell.pseudo:
+            pp_nl_nofield = pseudo.pp_int.get_pp_nl(cell, kpts)
+            pp_nl_nofield3 = get_gth_pp_nl_velgauge(cell, q=np.zeros(3), kpts=kpts, vgppnl_helper=self.vgppnl_helper)
+            ppnl_err = np.linalg.norm(pp_nl_nofield-pp_nl_nofield3)
+            if ppnl_err > 1e-5:
+                raise ValueError(f"ppnl_err1={ppnl_err}")
 
         v_ext = make_vext_velgauge(cell, afield, kpts, S, self.h1e_ipovlp, bc=bc, vgppnl_helper=self.vgppnl_helper)
-        fock_init = bc.rotate_focklike(h1e + my_get_veff(dm_kpts=dm) + pp_nl_nofield)
+
+
+        if cell.pseudo:
+            fock_init = bc.rotate_focklike(h1e + my_get_veff(dm_kpts=dm) + pp_nl_nofield)
+        else:
+            fock_init = bc.rotate_focklike(h1e + my_get_veff(dm_kpts=dm))
+
         dm = np.asarray(
             [np.diag(self._scf.mo_occ[k]) for k in range(nkpts)],
             dtype=np.complex128
