0e954859f2cfa33c75f97fda3b09fe48ab92130b
Christopher Hillenbrand <chillenbrand15@gmail.com>
2026-03-16 23:52:42 -0400

    print actual norm

--- src/rttddft/propagators/mpi_magnus2.py
+++ src/rttddft/propagators/mpi_magnus2.py
@@ -155,7 +155,8 @@ def step_magnus2(state, h1e, v_ext, S, get_veff, dt, conv_tol=1e-5, bc=None,
             F_p_half = 0.5 * (F + F_p_dt)
 
     if logger is not None:
-        logger.debug(f'Magnus2: time {t:.3f}, {nbuilds} get_veff call(s), |drho| = {diff:1.3e}')
+        difference_norm = np.linalg.norm(dm_p_dt - dm)
+        logger.debug(f'Magnus2: time {t:.3f}, {nbuilds} get_veff call(s), |drho| = {difference_norm:1.3e}')
 
     new_state = PropagatorState(
         dm=dm_p_dt,
